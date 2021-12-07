384b3718ecccdffe1a0be97dd036744d34614700
Gyh <gyh@gang-yeonhoui-MacBookAir.local>
2021-12-07 10:57:56 +0900

    google facebook login success

--- src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
@@ -9,6 +9,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.cos.security.config.auth.PrincipalDetails;
+import com.cos.security.config.oauth.provider.FacebookUserInfo;
+import com.cos.security.config.oauth.provider.GoogleUserInfo;
+import com.cos.security.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security.model.User;
 import com.cos.security.reposity.UserRepository;
 
@@ -25,16 +28,27 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		
 		OAuth2User oauth2User = super.loadUser(userRequest);
-
 		System.out.println("userRequest :" + userRequest.getClientRegistration());
 		System.out.println("userRequest :" + userRequest.getAccessToken().getTokenValue());
 		System.out.println("userRequest :" + oauth2User.getAttributes());
 		
-		String provider = userRequest.getClientRegistration().getClientId();
-		String providerId = oauth2User.getAttribute("sub");
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("google login");
+			oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+		}else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
+			System.out.println("facebook login");
+			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+		}else {
+			System.out.println("google or facebook only join");
+			
+		}
+		
+		String provider = oAuth2UserInfo.getProvider();
+		String providerId = oAuth2UserInfo.getProviderId();
 		String username = provider+"_"+providerId;
 		String password = bCryptPasswordEncoder.encode("비밀번호");
-		String email = oauth2User.getAttribute("email");
+		String email = oAuth2UserInfo.getEmail();
 		String role = "ROLE_USER";
 		
 		User userEntity = userRepository.findByUsername(username);
---
+++ src/main/java/com/cos/security/config/oauth/provider/GoogleUserInfo.java
@@ -0,0 +1,36 @@
+package com.cos.security.config.oauth.provider;
+
+import java.util.Map;
+
+public class GoogleUserInfo implements OAuth2UserInfo{
+	
+	private Map<String, Object> attributes;
+	
+	public GoogleUserInfo(Map<String, Object> attributes) {
+		this.attributes = attributes;
+	}
+	
+	@Override
+	public String getProviderId() {
+		// TODO Auto-generated method stub
+		return (String) attributes.get("sub");
+	}
+
+	@Override
+	public String getProvider() {
+		// TODO Auto-generated method stub
+		return "google";
+	}
+
+	@Override
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return (String) attributes.get("email");
+	}
+
+	@Override
+	public String getName() {
+		return (String) attributes.get("name");
+	}
+	
+}
